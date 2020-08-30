f0f5ce614d7c9cfc9bf6d7516a2bc2960ea21fc1
Brian Han <bmhan319@hotmail.com>
2020-08-30 13:41:34 -0700

    metronome js function complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+let play = false
+let myvar
+
 const metronome = () => {
-  console.log("tick")
+  const tickSound = document.getElementById("tick")
+
+  if (play === false) {
+    console.log("tick")
+    myvar = setInterval( () => {
+      tickSound.play()
+    }, 857)
+  } else if (play === true) {
+    console.log("Stop")
+    clearInterval(myvar)
+  }
+
+  play = !play
 }
