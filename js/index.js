6c06c4dc5da620ec5c10f423e27a0f9879a23ea1
Brian Han <bmhan319@hotmail.com>
2020-08-30 15:56:01 -0700

    figured out how to add first beat with an accent

--- js/index.js
+++ js/index.js
@@ -31,7 +31,6 @@ const metronome = (bpm) => {
 //BPM to MS conversion function
 const convert = (bpm) => {
     let time = ( (60 / bpm) * 1000 ).toFixed(2)
-    console.log(time)
     return time
   }
 
@@ -41,15 +40,12 @@ let play2 = false
 let isTicking2
 
 const metronome2 = (bpm, beats) => {
-  const first = document.getElementById("first")
+  const millisec = convert(bpm)
 
   //Start metronome
   if (play2 === false) {
-    setTimeout( () => {first.play()}, 0 )
-    setTimeout( () => {rest.play()}, 857 )
-    setTimeout( () => {rest.play()}, 1714 )
-    setTimeout( () => {rest.play()}, 2571 )
-    isTicking2 = setInterval( () =>{timer(beats)} , convert(bpm) * beats )
+    timer(millisec, beats)
+    isTicking2 = setInterval( () => { timer(millisec, beats) } , millisec * beats )
   
   //Stop metronome
   } else if (play2 === true) {
@@ -59,11 +55,11 @@ const metronome2 = (bpm, beats) => {
   play2 = !play2
 }
 
-function timer(num) {
+function timer(millisec, beats) {
   const first = document.getElementById("first")
   const rest = document.getElementById("rest")
   setTimeout( () => {first.play()}, 0 )
-  setTimeout( () => {rest.play()}, 857 )
-    setTimeout( () => {rest.play()}, 1714 )
-    setTimeout( () => {rest.play()}, 2571 )
+  setTimeout( () => {rest.play()}, millisec )
+  setTimeout( () => {rest.play()}, millisec * 2 )
+  setTimeout( () => {rest.play()}, millisec * 3 )
 }
