cb527586d40cc548e8fc2dadee234dec577b9a0f
Brian Han <bmhan319@hotmail.com>
2020-08-30 15:41:07 -0700

    working on alternate metronome function to play accent note on first beat

--- js/index.js
+++ js/index.js
@@ -31,5 +31,39 @@ const metronome = (bpm) => {
 //BPM to MS conversion function
 const convert = (bpm) => {
     let time = ( (60 / bpm) * 1000 ).toFixed(2)
+    console.log(time)
     return time
   }
+
+
+//Trying to fugure out how to add accent
+let play2 = false
+let isTicking2
+
+const metronome2 = (bpm, beats) => {
+  const first = document.getElementById("first")
+
+  //Start metronome
+  if (play2 === false) {
+    setTimeout( () => {first.play()}, 0 )
+    setTimeout( () => {rest.play()}, 857 )
+    setTimeout( () => {rest.play()}, 1714 )
+    setTimeout( () => {rest.play()}, 2571 )
+    isTicking2 = setInterval( () =>{timer(beats)} , convert(bpm) * beats )
+  
+  //Stop metronome
+  } else if (play2 === true) {
+    clearInterval(isTicking2)
+  }
+
+  play2 = !play2
+}
+
+function timer(num) {
+  const first = document.getElementById("first")
+  const rest = document.getElementById("rest")
+  setTimeout( () => {first.play()}, 0 )
+  setTimeout( () => {rest.play()}, 857 )
+    setTimeout( () => {rest.play()}, 1714 )
+    setTimeout( () => {rest.play()}, 2571 )
+}
