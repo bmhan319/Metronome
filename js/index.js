77e5eff924d66ef43dd9fd6fb59efc4e7fa50e11
Brian Han <bmhan319@hotmail.com>
2020-08-30 16:27:43 -0700

    cleaned up js code

--- js/index.js
+++ js/index.js
@@ -5,18 +5,21 @@
 //667ms  =  .667sec =  90bpm
 //500ms  =    .5sec = 120bpm
 
+
 //Global Variables
 let play = false
 let isTicking
 
+
 //Metronome function
-const metronome = (bpm) => {
-  const tickSound = document.getElementById("tick")
+const metronome = (bpm, beats) => {
+  const millisec = convert(bpm)
   const button = document.getElementById("playButton")
 
   //Start metronome
-  if (play === false) { 
-    isTicking = setInterval( () => {tickSound.play()}, convert(bpm) )
+  if (play === false) {
+    timer(millisec, beats)
+    isTicking = setInterval( () => { timer(millisec, beats) } , millisec * beats )
     button.innerHTML = "Stop"
   
   //Stop metronome
@@ -28,42 +31,22 @@ const metronome = (bpm) => {
   play = !play
 }
 
-//BPM to MS conversion function
+//BeatsPerMinute to Milliseconds conversion
 const convert = (bpm) => {
-    let time = ( (60 / bpm) * 1000 ).toFixed(2)
-    return time
+    let milliseconds = ( (60 / bpm) * 1000 ).toFixed(2)
+    return milliseconds
   }
 
-
-//Trying to fugure out how to add accent
-let play2 = false
-let isTicking2
-
-const metronome2 = (bpm, beats) => {
-  const millisec = convert(bpm)
-
-  //Start metronome
-  if (play2 === false) {
-    timer(millisec, beats)
-    isTicking2 = setInterval( () => { timer(millisec, beats) } , millisec * beats )
-  
-  //Stop metronome
-  } else if (play2 === true) {
-    clearInterval(isTicking2)
-  }
-
-  play2 = !play2
-}
-
+//Function to play beats at rate and speed given by user
 function timer(millisec, beats) {
-  const first = document.getElementById("first")
-  const rest = document.getElementById("rest")
-  
+  const accent = document.getElementById("accent")
+  const tick = document.getElementById("tick")
+
   for (var i = 0; i < beats; i++) {
     if (i === 0) {
-      first.play()
+      accent.play()
     } else {
-      setTimeout( () => {rest.play()}, millisec * i )
+      setTimeout( () => {tick.play()}, millisec * i )
     }
   }
 }
